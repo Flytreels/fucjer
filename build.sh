a7f7fa5b6eeff27a2694bddcd6ec5a578dcc60da
Your Name <you@example.com>
2021-02-09 22:04:42 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
-make -j$(nproc --all) O=out \
-ARCH=arm64 \
-CC=/usr/bin/gcc \
-CROSS_COMPILE=/usr/bin/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=/usr/bin/arm-linux-gnueabi- \
-LD=/root/tools/ld.lld \
+make O=out ARCH=arm64
