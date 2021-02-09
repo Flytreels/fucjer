45edb71c7f67cbbb60806c56a028168c035ae9b1
Your Name <you@example.com>
2021-02-09 23:25:19 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 cd ./android_kernel_huawei_kiwi/
-cat .config
+cp ../kiwi-64_defconfig ./arch/arm64/configs/kiwi-64_defconfig
+chmod 0770 ./arch/arm64/configs/kiwi-64_defconfig
 mkdir out
-make O=out ARCH=arm64
+make O=out kiwi-64_defconfig
+cd out
+make O=out 
 cd ..
