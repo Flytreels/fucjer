66adbac8183a50082f58cc79beda685e427c1a2a
Flytreels <66539732+Flytreels@users.noreply.github.com>
2021-02-10 10:16:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
-cd ./android_kernel_huawei_kiwi/
-cp ../kiwi-64_defconfig ./arch/arm64/configs/kiwi-64_defconfig
-chmod 0770 ./arch/arm64/configs/kiwi-64_defconfig
-mkdir out
-make O=out ARCH=arm64 kiwi-64_defconfig
-cd out
-make O=out 
-cd ..
+echo "root:1"|chpasswd
+echo "faq:1"|chpasswd
